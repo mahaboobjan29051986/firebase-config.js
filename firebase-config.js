4f9d1c3b977cd5d84c51e8dc59b025043d7c8276
mahaboobjan29051986 <mahaboobjan29051986@gmail.com>
2025-11-16 21:06:38 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,12 @@
-// Replace with your actual Firebase config
-
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyDrYtvOryuBEGFpwbSJe1ysXUCYIdKng6U",
   authDomain: "aptacosmetics.firebaseapp.com",
   databaseURL: "https://aptacosmetics-default-rtdb.firebaseio.com",
   projectId: "aptacosmetics",
   storageBucket: "aptacosmetics.firebasestorage.app",
   messagingSenderId: "45277127502",
-  appId: "YOUR_APP_ID"
+  appId: "1:45277127502:web:9e84d7f788770cbfd4f41f",
+  measurementId: "G-JTC5M6G2MB"
 };
 
 firebase.initializeApp(firebaseConfig);
